776862227cd265b2196a1b3c1ba26a983ee2b883
redell <382663074@qq.com>
2018-09-10 12:26:57 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <home-header />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '@/components/Header'
+export default {
+  name: 'App',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+  <div class="header">
+    <div class="header-left">
+        <span class="iconfont">&#xe61b;</span>
+    </div>
+    <div class="header-center">
+      <span class="iconfont">&#xe680;</span>
+      <span class="iconfont">&#xe674;</span>
+      <span class="iconfont">&#xe6f6;</span>
+    </div>
+    <div class="header-right">
+      <span class="iconfont">&#xe678;</span>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style lang="stylus">
+  @import '~styles/varibles.styl'
+    .header
+      display flex
+      justify-content space-between
+      align-items center
+      background #DF433A
+      height 45px
+      width 100%
+      .header-left
+        display flex
+        justify-content center
+        align-items center
+        width px2Rem(50px)
+        font-size px2Rem(24px)
+      .header-center
+        display flex
+        justify-content space-between
+        width px2Rem(120px)
+      .header-right
+        display flex
+        justify-content center
+        align-items center
+        width px2Rem(50px)
+        font-size px2Rem(24px)
+</style>
